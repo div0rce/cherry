710defa3172db50285518d5b2ee28e8b91d6789d
Mous <156965421+div0rce@users.noreply.github.com>
2026-01-28 13:10:43 -0500

    types: annotate nodemailer compat shim cleanup

--- types/compat/nodemailer.d.ts
+++ types/compat/nodemailer.d.ts
@@ -2,6 +2,7 @@
 // upstream: nodemailer@^7
 // audit: 2026-01-28
 // removeWhen: Remove once nodemailer ships transport typings or auth providers stop importing them
+// TODO(2026-01-28): Collapse transport shims to the minimal set actually referenced by the auth stack.
 declare module 'nodemailer' {
   export interface TransportOptions {
     [key: string]: unknown;
